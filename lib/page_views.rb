d68789a79b0b3dbe20a7ee1b05f2f1c7e182da1f
NoelleDL <noelle.legrain@gmail.com>
2019-02-04 17:45:47 +0000

    method for returning page views

--- lib/page_views.rb
+++ lib/page_views.rb
@@ -1,15 +1,29 @@
 class PageViews
 
+  attr_accessor :stats
+
   def initialize(log_file_address)
     @log_file_address = log_file_address
+    @stats = stats
   end
 
   def get_view_count(log_file_address)
+    stats = {}
 
     f = File.open(log_file_address, "r")
       f.each_line do |line|
-        puts line
+        address = line.match("^\/.* ").to_s.strip.split('/')[1];
+        if stats[address] == nil
+          stats[address] = 1
+        else
+          stats[address] += 1
+        end
       end
-    end
 
+      stats
+
+    end
   end
+
+  run_file = PageViews.new('./webserver.log')
+  puts run_file.get_view_count('./webserver.log')
